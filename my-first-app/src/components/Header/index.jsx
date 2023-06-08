a2af1b97750b29fcfa5a6e297a85da503b1d8cc2
ehteht487 <92257659+ehteht487@users.noreply.github.com>
2023-06-08 22:51:10 +0200

    Added header and nav

---
+++ my-first-app/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Nav } from "./Nav"; 
+
+export default function Header() {
+    return (
+        <header>
+            <Nav/>
+        </header>
+    );
+};
+
+
+
